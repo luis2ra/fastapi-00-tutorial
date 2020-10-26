ef2dea03521374b817e02fadde859b1402e20ebc
Luis Altuve <luis2ra@gmail.com>
2020-10-26 17:15:40 -0400

    tutorial: First Steps

--- main.py
+++ main.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World..."}
+
+
+'''
+Summary:
+
+* Import FastAPI.
+* Create an app instance.
+* Write a path operation decorator (like @app.get("/")).
+* Write a path operation function (like def root(): ... above).
+* Run the development server (like uvicorn main:app --reload).
+
+'''
