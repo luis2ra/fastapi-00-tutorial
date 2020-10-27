8df8f0ddf91da9805fff1db8943eeee4f02e1970
Luis Altuve <luis2ra@gmail.com>
2020-10-26 20:34:23 -0400

    tutorial: Path Parameters

--- main.py
+++ main.py
@@ -7,14 +7,32 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World..."}
 
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+@app.get("/items2/{item_id}")
+async def read_item2(item_id: int):
+    return {"item_id": item_id}
+
+@app.get("/users/me")
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
 
 '''
 Summary:
 
-* Import FastAPI.
-* Create an app instance.
-* Write a path operation decorator (like @app.get("/")).
-* Write a path operation function (like def root(): ... above).
-* Run the development server (like uvicorn main:app --reload).
+Path Parameters
+
+* You can declare path "parameters" or "variables" with the same syntax used by Python format strings.
+* You can declare the type of a path parameter in the function, using standard Python type annotations.
+* Data conversion / Data validation
+* Pydantic (For all the data validation)
+* Order matters (when creating path operations)
 
 '''
